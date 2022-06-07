2a53cd493264661680b163b9ad172640104d5206
Elakkuvan Rajamani <elakkuvanbecs@gmail.com>
2022-06-08 00:11:18 +0200

    Simple form component and a custom button component is added

--- src/Users/AddUser.js
+++ src/Users/AddUser.js
@@ -1,17 +1,22 @@
 import React from "react";
+import styles from "./AddUser.module.css";
+import Card from "../UI/Card";
+import Button from "../UI/Button";
 
 const AddUser = (props) => {
   const addUserHandler = (event) => {
     event.preventDefault();
   };
   return (
-    <form onSubmit={addUserHandler}>
-      <label htmlFor="username">User Name</label>
-      <input type="text" id="username"></input>
-      <label htmlFor="age">Age (Years)</label>
-      <input type="text" id="age"></input>
-      <button type="submit">Add User</button>
-    </form>
+    <Card className={styles.input}>
+      <form onSubmit={addUserHandler}>
+        <label htmlFor="username">User Name</label>
+        <input type="text" id="username"></input>
+        <label htmlFor="age">Age (Years)</label>
+        <input type="text" id="age"></input>
+        <Button type="submit">Add User</Button>
+      </form>
+    </Card>
   );
 };
 
